fd99278e4fdb941366fcfda3dcf27e79dde418c1
Courbin Francis <lezebre33560@gmail.com>
2017-04-28 10:25:11 +0200

    préparation URL API

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -1,9 +1,52 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET api listing. */
-router.get('/', (req, res) => {
-    res.send('api works');
-    });
+router.get('/', function (req, res) {
 
-    module.exports = router;
+    res.send('api works');
+});
+
+/**
+ * GET pour avoir toutes les questions existantes
+ */
+router.get('/questions', function (req, res) {
+    res.send("Coucou tu veux voir mes questions");
+});
+
+/**
+ * GET pour avoir une question
+ */
+router.get('/question/:id', function (req, res) {
+    // console.log(req);
+    // console.log(req.query);
+    // console.log(req.body);
+    // console.log(req.parms);
+
+    var params = req.params;
+    res.send("Coucou tu veux voir ma question n°" + params.id);
+});
+
+/**
+ * POST d'une question
+ */
+router.post('/question', function(req, res){
+
+    res.send("Coucou, tu créé une question");
+});
+
+/**
+ * POST d'une réponse à une question
+ */
+router.post('/question/:id/response', function(req, res){
+    var params = req.params;
+    res.send("Coucou, tu répond à une question dont l'id est " + params.id);
+});
+
+module.exports = router;
+
+/**
+ *  Ajouter une question
+ *  Récuperer la ou les qustions
+ *  répondre à une quzstion
+ */
--- express-server/server.js
+++ express-server/server.js
@@ -1,32 +1,36 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var bodyParser = require('body-parser');
 
 // Get our API routes
-const api = require('./routes/api');
+var api = require('./routes/api');
 
-const app = express();
+var app = express();
 
 // Parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // Set our api routes
 app.use('/', api);
 
 /**
  * Get port from environment and store in Express.
-  */
-  const port = process.env.PORT || '3000';
-  app.set('port', port);
+ */
+var port = process.env.PORT || '3000';
+app.set('port', port);
 
-  /**
-   * Create HTTP server.
-    */
-    const server = http.createServer(app);
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
 
-    /**
-     * Listen on provided port, on all network interfaces.
-      */
-      server.listen(port, () => console.log(`API running on localhost:${port}`));
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, function (){
+    console.log('API running on localhost:' + port);
+});
