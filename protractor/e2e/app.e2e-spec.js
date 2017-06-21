2a266cc03999c763b3ea3770870e2c01261f5a91
Courbin Francis <lezebre33560@gmail.com>
2017-06-21 11:53:22 +0200

    protractor test

---
+++ protractor/e2e/app.e2e-spec.js
@@ -0,0 +1,38 @@
+// import { ProjetAgilePage } from './app.po';
+// import { browser, element, by } from 'protractor';
+
+
+describe('projet-agile App', function() {
+
+  beforeEach(function() {
+    return browser.get('/');
+  });
+
+  it('GoodTitle', function() {
+    expect(browser.getTitle()).toEqual('ProjetAgile');
+  });
+
+  it('Go ask a question page', function() {
+      element(by.buttonText('Poser une question')).click();
+      expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "asking");
+      element(by.buttonText('Retour à l\'accueil')).click();
+      expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "main");
+  });
+
+  it('Go list question page', function() {
+      element(by.buttonText('Liste des questions')).click();
+      expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "questions");
+      element(by.buttonText('Retour à l\'accueil')).click();
+      expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "main");
+  });
+
+  it('Ask a question', function() {
+      element(by.buttonText('Liste des questions')).click();
+      expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "questions");
+      element(by.id("md-input-1")).sendKeys("Coucou");
+      element(by.buttonText('Envoyer')).click();
+      expect(element(by.id("response")).isPresent()).toBeTruthy();
+  });
+
+
+});
